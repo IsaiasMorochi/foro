a2c2e0565eb5e6005e919493b21772e398981887
Isaias Morochi <isaiasmorochi95@gmail.com>
2017-08-05 19:43:01 -0400

    commit preparacion

---
+++ tests/ExampleTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ExampleTest extends TestCase
+{
+
+    //use DatabaseMigrations;
+
+    //cuando tenemos mas de 30 migraciones para que demore en hacer la prueba y esta no ejecuta la migrate
+    // por esto hacer php artisan migrate=mysql_tests
+    use DatabaseTransactions;
+
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        $name = 'Isaias Morochi';
+        $email = 'admin@styde.net';
+
+        $user = factory(\App\User::class)->create([
+            'name' => $name,
+            'email' => $email,
+            ]);
+
+        $this->actingAs($user, 'api')
+             ->visit('api/user')
+             ->see($name)
+             ->see($email);
+    }
+}
